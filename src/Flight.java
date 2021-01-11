ce076b91c06bcf89259be1e2f3eb520b8a687c24
yfl410ylx <1695421675@qq.com>
2021-01-11 15:37:26 +0800

    Initial commit

---
+++ src/Flight.java
@@ -0,0 +1,47 @@
+public class Flight {
+    private String id;                   //ID
+    private String planeType;            // 飞机类型
+    private String departureAirport;     // 出发机场
+    private String destinationAirport;   // 目的机场
+    private String departureTime;        // 起飞时间
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPlaneType() {
+        return planeType;
+    }
+
+    public void setPlaneType(String planeType) {
+        this.planeType = planeType;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getDestinationAirport() {
+        return destinationAirport;
+    }
+
+    public void setDestinationAirport(String destinationAirport) {
+        this.destinationAirport = destinationAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+}
