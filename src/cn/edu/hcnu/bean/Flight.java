1ee9e9b8d0eb302307927170836da48822685897
Administrator <1234567@qq.com>
2021-01-12 17:03:53 +0800

    Java的分层架构建成

--- src/cn/edu/hcnu/bean/Flight.java
+++ src/cn/edu/hcnu/bean/Flight.java
@@ -1,7 +1,11 @@
+package cn.edu.hcnu.bean;
+
 //TODO 航班信息类
 public class Flight {        //飞行详情类
-    private String id;                   //ID
+    private String id;                   //主键UUID
+    private String flightId;                   //主键UUID
     private String planeType;            // 飞机类型
+    private int currentSeat;
     private String departureAirport;     // 出发机场
     private String destinationAirport;   // 目的机场
     private String departureTime;        // 起飞时间
--- src/cn/edu/hcnu/ui/Test.java
+++ src/cn/edu/hcnu/ui/Test.java
@@ -1,3 +1,5 @@
+package cn.edu.hcnu.ui;
+
 public class Test {
     public static void main(String[] args) {
 
